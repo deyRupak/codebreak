864cc2a54b29f0c4bf867d384dfb5628695c6fd3
preeya-g <priya181299@gmail.com>
2019-12-22 02:03:36 +0530

    modified contract

--- contracts/person.sol
+++ contracts/person.sol
@@ -7,34 +7,42 @@ uint public count;
 
 
 
-
 struct singledetails {
-    
+    string angutha;
     string name;
     string surname;
     string gender;
-    
+    bool accept;
 }
+
 
 mapping (address=>bool) public created;
 mapping (address => singledetails) public sabkidetails;  
 
-function adddetails( string memory _name,string memory _surname,string memory _gender) public onlyOwner{
+function adddetails
+(string memory _angutha,string memory _name,string memory _surname,string memory _gender) 
+public
+onlyOnce
+
+{
 
       count++;
-      sabkidetails[msg.sender]=singledetails(_name,_surname,_gender);
-      created[msg.sender]=true;
+      sabkidetails[msg.sender] = singledetails(_angutha,_name,_surname,_gender,false);
+      created[msg.sender] = true;
   } 
-  
-  
-  
-  
-    modifier onlyOwner {
+
+
+
+    function cansee(string memory _anguthachaap,address _personaddress) public{
+        if (keccak256(abi.encodePacked(_anguthachaap)) == keccak256(abi.encodePacked(sabkidetails[_personaddress].angutha))){
+    sabkidetails[_personaddress].accept = true;
+       }}
+
+ 
+    modifier onlyOnce {
         require(created[msg.sender] == false);
         _;
     }
 
-
-    
 }
 
